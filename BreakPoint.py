359fc94e0f5b02af378b65e37eda165c073736f5
xNullCode <79161850+DenebS4c@users.noreply.github.com>
2021-08-30 16:57:41 -0400

    Add files via upload

---
+++ BreakPoint.py
@@ -0,0 +1,136 @@
+# [+]==================[Creditos]==================[+]
+#  #                                                #
+#  #     Devs: Ghosty / xNullCode / Deneb           #
+#  #     Discord:                                   #
+#  #        Deneb007,#6666                          #
+#  #     Derechos de Author:                        #
+#  #         Ghosty / xNullCode / Deneb             #
+# [+]==================[Creditos]==================[+]
+#
+#                        |\**/|
+#                        \ == /
+#                         |  |
+#                         |  |
+#                         \  /
+#                          \/
+#
+# [+]==================[Start Code]==================[+]
+#
+# [+]==================[Imports]==================[+]
+
+import hashlib, sys, colorama, os
+from colorama import *
+
+# [+]==================[Imports]==================[+]
+
+
+# [+]==================[Extra]==================[+]
+
+if os.name != "nt":
+    os.system("clear")
+else:
+    os.system("title Developer » github.com/DenebS4c")
+    os.system("cls")
+    init(convert=True)
+
+# [+]==================[Extra]==================[+]
+
+
+# [+]==================[Banner]==================[+]
+
+def banner(sexo):
+    if sexo == "help":
+        print(rf"""
+                            {Fore.LIGHTCYAN_EX}ᶫᵒᵛᵉᵧₒᵤ
+    {Fore.LIGHTMAGENTA_EX}╭────────────────━━━━━━━━━━━━━━━━━━━━━━━━━────────────────╮
+    |{Fore.LIGHTCYAN_EX}              /\                                         {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}  /vvvvvvvvvvvv \--------------------------------------, {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}  `^^^^^^^^^^^^ /====================================="  {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}              \/                                         {Fore.LIGHTMAGENTA_EX}|
+    ╰────────────────━━━━━━━━━━━━━━━━━━━━━━━━━────────────────╯
+                            {Fore.LIGHTCYAN_EX}ᶫᵒᵛᵉᵧₒᵤ
+                               
+        {Fore.LIGHTMAGENTA_EX}╭────────────────━━━━━━━━━━━━━━━────────────────╮
+        | {Fore.LIGHTCYAN_EX}Use » python3 BreakPoint.py <hash> <wordlist> {Fore.LIGHTMAGENTA_EX}| 
+        ╰────────────────━━━━━━━━━━━━━━━────────────────╯
+                            {Fore.LIGHTCYAN_EX}ᶫᵒᵛᵉᵧₒᵤ
+        """)
+    else:
+        print(rf"""{Fore.LIGHTMAGENTA_EX}
+                            {Fore.LIGHTCYAN_EX}ᶫᵒᵛᵉᵧₒᵤ
+    {Fore.LIGHTMAGENTA_EX}╭────────────────━━━━━━━━━━━━━━━━━━━━━━━━━────────────────╮
+    |{Fore.LIGHTCYAN_EX}              /\                                         {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}  /vvvvvvvvvvvv \--------------------------------------, {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}  `^^^^^^^^^^^^ /====================================="  {Fore.LIGHTMAGENTA_EX}|
+    |{Fore.LIGHTCYAN_EX}              \/                                         {Fore.LIGHTMAGENTA_EX}|
+    ╰────────────────━━━━━━━━━━━━━━━━━━━━━━━━━────────────────╯
+                            {Fore.LIGHTCYAN_EX}ᶫᵒᵛᵉᵧₒᵤ
+        """)
+
+# [+]==================[Banner]==================[+]
+
+
+# [+]==================[Arguments]==================[+]
+
+if len(sys.argv) != 3:
+    banner(sexo="help")
+    sys.exit(-1)
+
+hash = sys.argv[1]
+wordlist = sys.argv[2]
+try:
+    hash2 = hash.split("$", 16)
+    hash = hash2[3]
+    salt = hash2[2]
+except:
+    hash2 = hash.split("$", 10)
+    hash = hash2[3]
+    salt = hash2[2]
+
+# [+]==================[Arguments]==================[+]
+
+
+# [+]==================[Read WordList]==================[+]
+
+banner(sexo="")
+print(f'        {Fore.LIGHTMAGENTA_EX}[ {Fore.LIGHTCYAN_EX}BreakPoint {Fore.LIGHTMAGENTA_EX}] {Fore.LIGHTCYAN_EX}Cargando Contraseñas de {Fore.LIGHTMAGENTA_EX}» {Fore.LIGHTCYAN_EX}{wordlist}')
+try:
+    wordlist = [x.strip() for x in open(file=wordlist, encoding='utf-8', mode='r', errors='ignore').readlines()]
+except:
+    print(f'        {Fore.LIGHTMAGENTA_EX}[ {Fore.LIGHTCYAN_EX}BreakPoint {Fore.LIGHTMAGENTA_EX}] {Fore.LIGHTCYAN_EX}Error Cargando Wordlist.')
+    sys.exit(-1)
+
+# [+]==================[Read WordList]==================[+]
+
+
+# [+]==================[Get Password]==================[+]
+
+intentos = 0 
+for password in wordlist:
+    intentos += 1
+    bruteforce = hashlib.sha256(hashlib.sha256(password.encode('utf-8')).hexdigest().encode('utf-8') + salt.encode('utf-8')).hexdigest()
+    if bruteforce == hash:
+        print(f'        {Fore.LIGHTMAGENTA_EX}[ {Fore.LIGHTCYAN_EX}BreakPoint {Fore.LIGHTMAGENTA_EX}] {Fore.LIGHTCYAN_EX}Contraseña Encontrada {Fore.LIGHTMAGENTA_EX}» {Fore.LIGHTCYAN_EX}{password}')
+        sys.exit(-1)
+print(f'        {Fore.LIGHTMAGENTA_EX}[ {Fore.LIGHTCYAN_EX}BreakPoint {Fore.LIGHTMAGENTA_EX}] {Fore.LIGHTCYAN_EX}Contraseña No Encontrada')
+
+# [+]==================[Get Password]==================[+]
+#
+#
+# [+]==================[End Code]==================[+]
+#
+#                          /\           
+#                         /  \    
+#                         |  |     
+#                         |  |     
+#                        / == \    
+#                        |/**\|  
+#    
+# [+]==================[Creditos]==================[+]
+#  #                                                #
+#  #     Devs: Ghosty / xNullCode / Deneb           #
+#  #     Discord:                                   #
+#  #        Deneb007,#6666                          #
+#  #     Derechos de Author:                        #
+#  #         Ghosty / xNullCode / Deneb             #
+# [+]==================[Creditos]==================[+]
